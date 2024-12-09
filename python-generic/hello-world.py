b615096aa6b79f6a0df1d72b3693319d60146d68
Ruairidh MacLeod <ruairidh@rkm.dev>
2024-12-09 10:19:56 +0000

    add generic python example

---
+++ python-generic/hello-world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def main() -> int:
+    x = np.array([[1, 2, 3], [4, 5, 6]])
+    print(x)
+    return 0
+
+if __name__ == '__main__':
+    raise SystemExit(main())

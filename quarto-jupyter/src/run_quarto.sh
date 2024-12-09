f2b460ecdbeae5856c95ab856e83428169a2180a
Ruairidh MacLeod <ruairidh@rkm.dev>
2024-12-09 14:15:40 +0000

    add quarto examples

---
+++ quarto-jupyter/src/run_quarto.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Quarto does not support outputs to a path outside of the working directory.
+# Workaround is to move output files after they are generated.
+quarto render hello.ipynb --to pdf
+mv *.pdf /safe_outputs

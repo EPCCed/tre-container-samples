bc007e6d32c5cdbb06adc9ecdda7415920a7b2d8
giuliadei <g.deiana@epcc.ed.ac.uk>
2025-01-29 17:09:36 +0000

    Adding octave example

---
+++ octave/src/plot_example.m
@@ -0,0 +1,31 @@
+% declaring variable var_x
+var_x = [0:0.01:1];
+ 
+% declaring variable var_y1
+var_y1 = sin(4 * pi * var_x);
+ 
+% declaring variable var_y2
+var_y2 = cos(3 * pi * var_x);
+ 
+% plot var_x with var_y1
+plot(var_x, var_y1);
+ 
+% hold the above plot or figure
+hold on;
+ 
+% plot var with var_y2 with red color
+plot(var_x, var_y2, 'r');
+ 
+% adding label to the x-axis
+xlabel('time');
+ 
+% adding label to the y-axis
+ylabel('value');
+ 
+% adding title for the plot
+title('my first plot');
+ 
+% add legends for these 2 curves
+legend('sin', 'cos');
+
+print -dpng 'plot.png'
---
+++ octave/src/run_octave.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+octave plot_example.m
+
+mv *.png /safe_outputs

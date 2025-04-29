0a07bb94cc740a2c92e4c739da50e96eb411b059
Ruairidh MacLeod <ruairidh@rkm.dev>
2025-04-29 13:40:27 +0100

    ensure non-root users can run quarto

--- quarto-jupyter/src/run_quarto.sh
+++ quarto-jupyter/src/run_quarto.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 # Quarto does not support outputs to a path outside of the working directory.
 # Workaround is to move output files after they are generated.
 quarto render hello.ipynb --to pdf
-mv ./*.pdf /safe_outputs
+cp ./*.pdf /safe_outputs

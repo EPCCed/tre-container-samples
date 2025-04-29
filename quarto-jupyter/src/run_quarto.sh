48505c976f6707bb56ea3ddb081d77928b3c2ba4
Ruairidh MacLeod <ruairidh@rkm.dev>
2025-04-29 11:58:24 +0100

    add bash switches

--- quarto-jupyter/src/run_quarto.sh
+++ quarto-jupyter/src/run_quarto.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 # Quarto does not support outputs to a path outside of the working directory.
 # Workaround is to move output files after they are generated.
 quarto render hello.ipynb --to pdf
